e491877c88a8649485fd352b3309be9f74c3a0d5
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-07-21 15:18:30 +0400

    testing prettier hook

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,1 +1,6 @@
 import "../scss/registration.scss";
+
+
+function kotik(a, b, c, d, asd, asfvere, xfv, dfgv, sdrt,gsdrg, sdfsdf, aaaaa, dddd, ss, eee, ff, vvv, ggg, kkkkk, dvvvsd, sdfvsdddddd, ccccc) {
+
+}
