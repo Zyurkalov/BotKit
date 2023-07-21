26c656e241d4ef8d83b4198f66a349b64dd75f49
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-07-21 15:27:51 +0400

    testing prettier

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,6 +1,28 @@
 import "../scss/registration.scss";
 
-
-function kotik(a, b, c, d, asd, asfvere, xfv, dfgv, sdrt,gsdrg, sdfsdf, aaaaa, dddd, ss, eee, ff, vvv, ggg, kkkkk, dvvvsd, sdfvsdddddd, ccccc) {
-
+function kotik(
+  a,
+  b,
+  c,
+  d,
+  asd,
+  asfvere,
+  xfv,
+  dfgv,
+  sdrt,
+  gsdrg,
+  sdfsdf,
+  aaaaa,
+  dddd,
+  ss,
+  eee,
+  ff,
+  vvv,
+  ggg,
+  kkkkk,
+  dvvvsd,
+  sdfvsdddddd,
+  ccccc,
+) {
+  console.log("Hi");
 }
