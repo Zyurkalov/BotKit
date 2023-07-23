90ff4d83524ac4b8843023546586029da578696d
edgar-ianke <edgaryanke@yandex.ru>
2023-07-23 09:29:29 +0300

    Index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import "./scss/navigation.scss"; // styles unique to index.html and ui-kit-navigation-page.html
-import "./scss/ui-kit/ui-kit-navigation.scss";
+import "./scss/ui-kit/ui_kit_navigation.scss";
