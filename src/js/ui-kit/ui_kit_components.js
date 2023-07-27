c17299cf2f65acb9aac04de2401569a1770c84dd
Ivan Shanin <ivanxshanin@gmail.com>
2023-07-27 20:40:17 +0300

    create scrollable list & multiple choice list

--- src/js/ui-kit/ui_kit_components.js
+++ src/js/ui-kit/ui_kit_components.js
@@ -1,1 +1,42 @@
 import "../../scss/ui-kit/ui_kit_components.scss";
+
+const checkmarks = Array.from(
+  document.querySelectorAll(".selector__checkmark"),
+);
+
+const allItemsOption = document.querySelector(".selector__item-all");
+
+const allItemsCheckmark = allItemsOption.querySelector(
+  ".selector__checkmark-for-all",
+);
+
+checkmarks.forEach((checkmark) => {
+  checkmark.closest(".selector__item").addEventListener("click", () => {
+    checkmark.classList.toggle("selector__checkmark_checked");
+    if (
+      checkmarks.every((checkmark) =>
+        checkmark.classList.contains("selector__checkmark_checked"),
+      )
+    ) {
+      allItemsCheckmark.classList.add("selector__checkmark-for-all_checked");
+    } else {
+      allItemsCheckmark.classList.remove("selector__checkmark-for-all_checked");
+    }
+  });
+});
+
+allItemsOption.addEventListener("click", () => {
+  if (
+    allItemsCheckmark.classList.contains("selector__checkmark-for-all_checked")
+  ) {
+    checkmarks.forEach((checkmark) => {
+      checkmark.classList.remove("selector__checkmark_checked");
+    });
+    allItemsCheckmark.classList.remove("selector__checkmark-for-all_checked");
+  } else {
+    checkmarks.forEach((checkmark) => {
+      checkmark.classList.add("selector__checkmark_checked");
+    });
+    allItemsCheckmark.classList.add("selector__checkmark-for-all_checked");
+  }
+});
