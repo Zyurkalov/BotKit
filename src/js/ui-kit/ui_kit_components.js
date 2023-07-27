27318f941642a05b449ce3bf34b685a536b6e23d
Ivan Shanin <ivanxshanin@gmail.com>
2023-07-27 22:12:24 +0300

    add documentation to javascript

--- src/js/ui-kit/ui_kit_components.js
+++ src/js/ui-kit/ui_kit_components.js
@@ -1,25 +1,32 @@
 import "../../scss/ui-kit/ui_kit_components.scss";
 
+// все галочки обычных опций
 const checkmarks = Array.from(
   document.querySelectorAll(".selector__checkmark"),
 );
 
+// отдельная опция "выбрать всё"
 const allItemsOption = document.querySelector(".selector__item-all");
 
+// галочка опции "выбрать всё"
 const allItemsCheckmark = allItemsOption.querySelector(
   ".selector__checkmark-for-all",
 );
 
 checkmarks.forEach((checkmark) => {
   checkmark.closest(".selector__item").addEventListener("click", () => {
+    // меняем состояние галочки
     checkmark.classList.toggle("selector__checkmark_checked");
     if (
+      // если после изменения все галочки оказались выбраны
       checkmarks.every((checkmark) =>
         checkmark.classList.contains("selector__checkmark_checked"),
       )
     ) {
+      // то добавляем галочку опции "выбрать всё"
       allItemsCheckmark.classList.add("selector__checkmark-for-all_checked");
     } else {
+      // иначе - снимаем галочку с опции "выбрать всё"
       allItemsCheckmark.classList.remove("selector__checkmark-for-all_checked");
     }
   });
@@ -27,16 +34,21 @@ checkmarks.forEach((checkmark) => {
 
 allItemsOption.addEventListener("click", () => {
   if (
+    // если отмечена галочка "выбрать всё"
     allItemsCheckmark.classList.contains("selector__checkmark-for-all_checked")
   ) {
+    // то убираем галочку со всех отдельных опций
     checkmarks.forEach((checkmark) => {
       checkmark.classList.remove("selector__checkmark_checked");
     });
+    // и с опции "выбрать всё" тоже
     allItemsCheckmark.classList.remove("selector__checkmark-for-all_checked");
   } else {
+    // иначе добавляем галочку всем отдельным опциям
     checkmarks.forEach((checkmark) => {
       checkmark.classList.add("selector__checkmark_checked");
     });
+    // и опции "выбрать всё" тоже
     allItemsCheckmark.classList.add("selector__checkmark-for-all_checked");
   }
 });
