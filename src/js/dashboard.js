26b1b84dbd46747b52fe00ee0cc496bb4f90b0aa
Alexey Sokolov <S25keeper@yandex.ru>
2023-07-28 19:26:00 +0300

    fix/style: обновить стили меню + основа страницы (шапка, подвал и тело) через grid

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,1 +1,9 @@
 import "../scss/dashboard/dashboard.scss";
+
+import { closeMenu } from "./ui-kit/ui_kit_navigation";
+
+const page = document.querySelector(".page");
+const header = page.querySelector(".header");
+const headerMenu = header.querySelector(".header__menu");
+
+headerMenu.addEventListener("click", () => closeMenu());
--- src/js/ui-kit/ui_kit_navigation.js
+++ src/js/ui-kit/ui_kit_navigation.js
@@ -1,1 +1,17 @@
 import "../../scss/ui-kit/ui_kit_navigation.scss";
+
+const page = document.querySelector(".page");
+const mainMenu = page.querySelector(".menu");
+
+export function closeMenu() {
+  mainMenu.classList.toggle("menu-close");
+  mainMenu
+    .querySelector(".link_theme_blue")
+    .classList.toggle("link_theme_blue-close");
+  mainMenu
+    .querySelector(".link__image_reverse")
+    .classList.toggle("link__image_reverse-close");
+  mainMenu
+    .querySelector(".link__text_reverse")
+    .classList.toggle("link__text_reverse-close");
+}
