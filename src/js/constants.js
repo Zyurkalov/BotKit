bd4f10f6fdaf25d31dfc69f45b26519bbc3537b5
triple6_2la <rahim_dovidov@mail.ru>
2023-07-31 17:16:14 +0300

    feat: add popup open/close by JS

--- src/js/constants.js
+++ src/js/constants.js
@@ -5,6 +5,12 @@ const selectors = {
   loginPage: {
     signUpBtn: ".passw-recovery__link_type_register",
   },
+  dashboardPage: {
+    addBotPopup: "add-bot-popup",
+    addBotOpenBtn: "panel-add-bot",
+    addBotCloseBtn: "add-bot-popup__close-btn",
+    hiddenPopupCls: "add-bot-popup_hidden",
+  },
 };
 
 const routes = {
--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,3 +1,38 @@
 import "../scss/dashboard/dashboard.scss";
-
+import { selectors } from "./constants";
 import { closeMenu } from "./ui-kit/ui_kit_navigation";
+
+const botPopup = document.querySelector(
+  "." + selectors.dashboardPage.addBotPopup,
+);
+const botPopupCloseBtn = botPopup.querySelector(
+  "." + selectors.dashboardPage.addBotCloseBtn,
+);
+const botPopupOpenBtn = document.querySelector(
+  "." + selectors.dashboardPage.addBotOpenBtn,
+);
+
+function openPopup() {
+  botPopup.classList.remove(selectors.dashboardPage.hiddenPopupCls);
+  document.addEventListener("keydown", onEscClose);
+}
+
+function onOverlayClick(evt) {
+  if (evt.target.classList.contains(selectors.dashboardPage.addBotPopup)) {
+    closePopup();
+  }
+}
+
+function onEscClose(evt) {
+  if (evt.key === "Escape") {
+    closePopup();
+  }
+}
+function closePopup() {
+  botPopup.classList.add(selectors.dashboardPage.hiddenPopupCls);
+  document.removeEventListener("keydown", onEscClose);
+}
+
+botPopup.addEventListener("click", onOverlayClick);
+botPopupCloseBtn.addEventListener("click", closePopup);
+botPopupOpenBtn.addEventListener("click", openPopup);
