31a272ca8c39184cd858e67f68bb95d9ec25b111
Nolik13 <vasia200257@gmail.com>
2023-07-31 18:35:06 +1000

    Transfer to another file and add eventList for button

--- src/js/reset_password.js
+++ src/js/reset_password.js
@@ -1,1 +1,9 @@
 import "../scss/reset-password/reset_password.scss";
+import { routes, selectors } from "./constants";
+
+const signInBtn = document.querySelector(selectors.resetPasswordPage.resetBtn);
+function redirectToPage() {
+  window.location.href = window.location.origin + routes.regPage;
+}
+
+signInBtn.addEventListener("click", redirectToPage);
