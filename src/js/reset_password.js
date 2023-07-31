3a19216fd2a0742812a12a7636f2ddfd879fb3b9
Nolik13 <vasia200257@gmail.com>
2023-07-31 21:05:03 +1000

    RegistrationPage, ConfirmationPage, eventListen for buttons

--- src/js/reset_password.js
+++ src/js/reset_password.js
@@ -2,8 +2,24 @@ import "../scss/reset-password/reset_password.scss";
 import { routes, selectors } from "./constants";
 
 const signInBtn = document.querySelector(selectors.resetPasswordPage.resetBtn);
-function redirectToPage() {
-  window.location.href = window.location.origin + routes.regPage;
+const page = document.querySelector(".page");
+const header = document.querySelector(".header");
+const popup = document.querySelector(".window");
+const main = document.querySelector(".main");
+const reset_passwordPage = document.querySelector(".resetPassword");
+
+function openAndClosePage(evt) {
+  evt.preventDefault();
+  page.classList.toggle("page_newMeasure");
+  header.classList.toggle("header_inActive");
+  popup.classList.toggle("window_inActive");
+  main.classList.toggle("main_newMeasure");
+  reset_passwordPage.classList.toggle("resetPassword_active");
 }
 
-signInBtn.addEventListener("click", redirectToPage);
+signInBtn.addEventListener("click", openAndClosePage);
+// Закрытие
+
+const closeBtn = document.querySelector(".resetPassword__close-btn");
+
+closeBtn.addEventListener("click", openAndClosePage);
