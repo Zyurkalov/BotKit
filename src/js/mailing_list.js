662d48581f8edfd526e75481bba5f5b3e45f2352
Ivan Shanin <ivanxshanin@gmail.com>
2023-08-02 18:55:41 +0300

    create mailings page with content depending on mailings existence

--- src/js/mailing_list.js
+++ src/js/mailing_list.js
@@ -2,11 +2,18 @@ import "../scss/mailing-list/mailing_list.scss";
 
 import { closeMenu } from "./ui-kit/ui_kit_navigation.js";
 
-class Mailing {
+const mailingIdSelector = ".mailing-list__cell_data_id";
+const mailingNameSelector = ".mailing-list__cell_data_name";
+const mailingMessengerSelector = ".mailing-list__cell_data_messenger";
+const mailingSentMessagesSelector = ".mailing-list__cell_data_sent-messages";
+const mailingConversionRateSelector = ".mailing-list__cell_data_conversion";
+const mailinglaunchStatusSelector = ".mailing-list__cell_data_launch-status";
+
+export class Mailing {
   constructor(
     { id, name, messenger, sentMessages, conversionRate, launchStatus },
-    containerSelector,
-    templateSelector,
+    tableBodySelector,
+    mailingTemplateSelector,
   ) {
     this._id = id;
     this._name = name;
@@ -14,18 +21,18 @@ class Mailing {
     this._sentMessages = sentMessages;
     this._conversionRate = conversionRate;
     this._launchStatus = launchStatus;
-    this._container = document.querySelector(containerSelector);
-    this._template = document.querySelector(templateSelector).content;
+    this._container = document.querySelector(tableBodySelector);
+    this._template = document.querySelector(mailingTemplateSelector).content;
   }
 
-  _getElement() {
+  _getElement(tableRowSelector) {
     this._element = this._template
-      .querySelector(mailingRowSelector)
+      .querySelector(tableRowSelector)
       .cloneNode(true);
   }
 
   _formatSentMessages() {
-    const lastDigit = this._sentMessages.toString()[-1];
+    const lastDigit = this._sentMessages.toString().slice(-1);
     let entity = "сообщений";
 
     switch (lastDigit) {
@@ -40,6 +47,7 @@ class Mailing {
         break;
       case "4":
         entity = "сообщения";
+        break;
     }
 
     this._sentMessages += " ";
@@ -57,7 +65,7 @@ class Mailing {
     this._element.querySelector(mailingMessengerSelector).textContent =
       this._messenger;
     this._element.querySelector(mailingSentMessagesSelector).textContent =
-      this._sentMessagesString;
+      this._sentMessages;
     this._element.querySelector(mailingConversionRateSelector).textContent =
       this._conversionRate;
     this._element.querySelector(mailinglaunchStatusSelector).textContent =
@@ -68,12 +76,12 @@ class Mailing {
     if (this._launchStatus === "Отклонено") {
       this._element
         .querySelector(mailinglaunchStatusSelector)
-        .classList.add("_rejected");
+        .classList.add("mailing-list__cell_data_launch-status_rejected");
     }
   }
 
-  generate() {
-    this._getElement();
+  generate(tableRowSelector) {
+    this._getElement(tableRowSelector);
 
     this._formatSentMessages();
     this._formatConversionRate();
@@ -89,27 +97,59 @@ class Mailing {
   }
 }
 
-// в будущем будет так:
-// mailingsList = fetch(...);
+// в будущем будем запрашивать данные с сервера:
+// const mailingsList = fetch(...);
+
 // а пока - так:
-mailingsList = [
+const mailingsList = [
   {
     id: 12345678,
     name: "№1",
+    messenger: "Telegram",
+    sentMessages: 10,
+    conversionRate: 0.446,
     launchStatus: "Запущено",
   },
   {
-    id: 12345678,
+    id: 1234567,
     name: "№2",
+    messenger: "Discord",
+    sentMessages: 34,
+    conversionRate: 0,
     launchStatus: "Отклонено",
   },
   {
-    id: 12345678,
+    id: 123456,
     name: "№3",
+    messenger: "Viber",
+    sentMessages: 0,
+    conversionRate: 1,
     launchStatus: "Запущено",
   },
 ];
 
+const mailingWindow = document.querySelector(".mailing-window");
+
+const mailingListSection = mailingWindow.querySelector(".mailing-list");
+const mailingListTutorials = mailingWindow.querySelector(".mailing-tutorials");
+const mailingListPlaceholder = mailingWindow.querySelector(
+  ".mailing-list-placeholder",
+);
+
+// можно проверить, что при пустом списке показывается другой блок
+if (mailingsList.length == 0) {
+  mailingWindow.classList.add("mailing-window_centered");
+  mailingListSection.classList.remove("mailing-list_visible");
+  mailingListTutorials.classList.remove("mailing-tutorials_visible");
+  mailingListPlaceholder.classList.add("mailing-list-placeholder_visible");
+}
+
 mailingsList.forEach((mailingObject) => {
-  const mailing = new Mailing(mailingObject, "", "");
+  const mailing = new Mailing(
+    mailingObject,
+    ".mailing-list__body",
+    "#mailing-template",
+  );
+  mailing.generate(".mailing-list__row");
+  mailing.insert();
 });
