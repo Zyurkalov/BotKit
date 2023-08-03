3998b612f34753d2d79e029f19bf0d43ada6ac67
HockeyBear <nikita.efremov.02@mail.ru>
2023-08-03 10:29:11 +0500

    pp законченный

--- src/js/pp.js
+++ src/js/pp.js
@@ -1,2 +1,13 @@
 import "../scss/pp/pp.scss";
 import "./ui-kit/ui_kit_navigation";
+
+const statsButtonUp = document.querySelector(".stats-up-button");
+const ppWindowContainer = document.querySelector(".pp-window__container");
+const statsPayout = document.querySelector(".table-payout");
+
+function toggleStatsClosedOpen() {
+  ppWindowContainer.classList.toggle("pp-window__container-closed");
+  statsPayout.classList.toggle("table-payout-closed");
+}
+
+statsButtonUp.addEventListener("click", toggleStatsClosedOpen);
